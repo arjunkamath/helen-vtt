7beed0a36cd9722957d6a7af58999ecf6a29bac3
Arjun <junnaonly@gmail.com>
2016-11-29 20:35:54 +0200

    websocket starting point

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var https = require('https');
 
+const PORT = process.env.PORT || 3000;
+
 var data_ella = JSON.stringify({
   api_key: 'e5f4990f',
   api_secret: 'nexmo123',
@@ -11,75 +13,6 @@ var data_ella = JSON.stringify({
   from: '+358442992809',
   text: 'VEHO: Input needed for service veho-mb.eu-gb.mybluemix.net/customer '
 });
-
-// var data_parth = JSON.stringify({
-//   api_key: 'e5f4990f',
-//   api_secret: 'nexmo123',
-//   to: '+358452612720',
-//   from: '+358442992809',
-//   text: 'VEHO: Input needed for service veho-mb.eu-gb.mybluemix.net/customer '
-// });
-
-// var data_ville = JSON.stringify({
-//   api_key: 'e5f4990f',
-//   api_secret: 'nexmo123',
-//   to: '+358408619447',
-//   from: '+358442992809',
-//   text: 'VEHO: Input needed for service veho-mb.eu-gb.mybluemix.net/customer '
-// });
-
-// var data_mikael = JSON.stringify({
-//   api_key: 'e5f4990f',
-//   api_secret: 'nexmo123',
-//   to: '+358503227007',
-//   from: '+358442992809',
-//   text: 'VEHO: Input needed for service veho-mb.eu-gb.mybluemix.net/customer '
-// });
-
-
-
-
-
-
-// var data_ilkka = JSON.stringify({
-//   api_key: 'e5f4990f',
-//   api_secret: 'nexmo123',
-//   to: '+358407275856',
-//   from: '+358442992809',
-//   text: 'VEHO: Input needed for service veho-mb.eu-gb.mybluemix.net/customer '
-// });
-
-// var data_kenneth = JSON.stringify({
-//   api_key: 'e5f4990f',
-//   api_secret: 'nexmo123',
-//   to: '+358400458395',
-//   from: '+358442992809',
-//   text: 'VEHO: Input needed for service veho-mb.eu-gb.mybluemix.net/customer '
-// });
-
-// var data_hannu = JSON.stringify({
-//   api_key: 'e5f4990f',
-//   api_secret: 'nexmo123',
-//   to: '+358405488489',
-//   from: '+358442992809',
-//   text: 'VEHO: Input needed for service veho-mb.eu-gb.mybluemix.net/customer '
-// });
-
-// var data_kari = JSON.stringify({
-//   api_key: 'e5f4990f',
-//   api_secret: 'nexmo123',
-//   to: '+358505464150',
-//   from: '+358442992809',
-//   text: 'VEHO: Input needed for service veho-mb.eu-gb.mybluemix.net/customer '
-// });
-
-// var data_jarno = JSON.stringify({
-//   api_key: 'e5f4990f',
-//   api_secret: 'nexmo123',
-//   to: '+358504357165',
-//   from: '+358442992809',
-//   text: 'VEHO: Input needed for service veho-mb.eu-gb.mybluemix.net/customer '
-// });
 
 var options = {
   host: 'rest.nexmo.com',
@@ -91,16 +24,6 @@ var options = {
     'Content-Length': Buffer.byteLength(data_ella)
   }
 };
-
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
-// serve the files out of ./public as our main files
-//app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
 
 //get css files
 app.use(express.static('public'));
@@ -138,27 +61,10 @@ app.get('/logistics', function(req, res){
 // });
 
 io.on('connection', function(socket){
-  //console.log('someone connected 2');
 
   socket.on('feedback', function(feedback){
     io.emit('feedback', feedback);
-	  //msgs.emit('feedback', feedback);
-	  // console.log(feedback);
-   //    console.log('should SMS be sent');
 
-   //  if(feedback['send_sms']){
-   //    console.log('SMS to be sent to all');
-      
-
-   //     // nexmo_sms_send(data_ella);
-   //     // nexmo_sms_send(data_parth);
-   //     // nexmo_sms_send(data_ilkka);
-   //     // nexmo_sms_send(data_kari);
-   //     // nexmo_sms_send(data_hannu);
-   //     // nexmo_sms_send(data_kenneth);
-   //     // nexmo_sms_send(data_jarno);
-
-   //  }
   });
 });
 
@@ -183,12 +89,7 @@ function nexmo_sms_send(data_to_send){
 }
 
 
-http.listen(appEnv.port, function(){
-  console.log("server starting on " + appEnv.url);
+http.listen(PORT, function(){
+  console.log("server starting on " + PORT);
 });
 
-// start server on the specified port and binding host
-//app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  //console.log("server starting on " + appEnv.url);
-//});
